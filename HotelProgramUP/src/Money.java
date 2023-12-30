6a09b53033306d09103c07ba124564ddf1ecd204
main <main@192.168.219.100>
2023-12-31 02:22:26 +0900

    수동입력결제 -> 자동결제

---
+++ HotelProgramUP/src/Money.java
@@ -0,0 +1,17 @@
+public class Money {
+	
+	
+	private int money;
+
+    public Money(int money) {
+        this.money = money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+}
